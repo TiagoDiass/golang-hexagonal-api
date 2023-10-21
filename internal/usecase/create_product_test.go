2ae49fa46fb834e394377b071cc52bc9791ba5ad
TiagoDiass <tiago.costadiasss@gmail.com>
2023-10-21 14:22:20 -0300

    test: CreateProductUsecase tests

---
+++ internal/usecase/create_product_test.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"testing"
+
+	"github.com/TiagoDiass/golang-hexagonal-api/internal/entity"
+	"github.com/TiagoDiass/golang-hexagonal-api/internal/test"
+	"github.com/TiagoDiass/golang-hexagonal-api/internal/test/assertions"
+)
+
+func TestShouldCreateProductSuccessfully(t *testing.T) {
+	t.Parallel()
+
+	productRepository := test.NewInMemoryProductRepository()
+	createProductUsecase := NewCreateProductUsecase(productRepository)
+
+	productRepository.Create(&entity.Product{
+		ID:    "fake-product-id",
+		Name:  "fake-product",
+		Price: 100,
+	})
+
+	input := CreateProductInputDTO{
+		Name:  "fake-product-name",
+		Price: 1500,
+	}
+
+	output, err := createProductUsecase.Execute(input)
+
+	if err != nil {
+		t.Error("Expected not to receive an error from CreateProductUsecase")
+	}
+
+	assertions.ExpectToBe(t, output.Name, "fake-product-name")
+	assertions.ExpectToBe(t, output.Price, 1500)
+}
