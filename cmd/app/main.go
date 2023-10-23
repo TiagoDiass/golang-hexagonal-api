7528e927b41ac0195d729474dcc6340b41bcb4b9
TiagoDiass <tiago.costadiasss@gmail.com>
2023-10-22 23:40:53 -0300

    refactor: organize code in main.go

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,28 +4,40 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	_ "github.com/go-sql-driver/mysql"
+
 	repositoryimpl "github.com/TiagoDiass/golang-hexagonal-api/internal/infra/repository_impl"
 	"github.com/TiagoDiass/golang-hexagonal-api/internal/infra/web"
 	"github.com/TiagoDiass/golang-hexagonal-api/internal/usecase"
-	"github.com/go-chi/chi/v5"
-	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306)/products")
-
-	db.Exec(`create table if not exists products (
-		id varchar(255), 
-		name varchar(255), 
-		price integer,
-		PRIMARY KEY (id)
-	);`)
 
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
+	CreateProductsTableIfNotExists(db)
+
+	productHandlers := CreateProductHandlers(db)
+	router := InitializeRoutes(productHandlers)
+
+	http.ListenAndServe(":8000", router)
+}
+
+func CreateProductsTableIfNotExists(db *sql.DB) {
+	db.Exec(`create table if not exists products (
+		id varchar(255), 
+		name varchar(255), 
+		price integer,
+		PRIMARY KEY (id)
+	);`)
+}
+
+func CreateProductHandlers(db *sql.DB) *web.ProductHandlers {
 	productRepository := repositoryimpl.NewProductRepositoryMysql(db)
 	createProductUsecase := usecase.NewCreateProductUsecase(productRepository)
 	listProductsUsecase := usecase.NewListProductsUsecase(productRepository)
@@ -39,6 +51,10 @@ func main() {
 		findProductByIdUsecase,
 	)
 
+	return productHandlers
+}
+
+func InitializeRoutes(productHandlers *web.ProductHandlers) *chi.Mux {
 	router := chi.NewRouter()
 
 	router.Get("/products", productHandlers.ListProductsHandler)
@@ -46,5 +62,5 @@ func main() {
 	router.Get("/products/{productId}", productHandlers.FindProductByIdHandler)
 	router.Delete("/products/{productId}", productHandlers.DeleteProductHandler)
 
-	http.ListenAndServe(":8000", router)
+	return router
 }
